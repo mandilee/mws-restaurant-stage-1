694cfb361651843ca087679b02db479221af11ac
Mandi Lee <github@mandilee.co.uk>
2018-02-25 18:04:26 +0000

    feat: Make index.html responsive and pretty
    Use flexbox and wrap on restaurant list to fill the screen with 1-5
    cards depending on the size of the display.
    Add grid on the restaurant cards to make them uniform.
    Tweak the header and filter styles to make it pretty.

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-  li.className = 'list-restaurant-container';
+  li.className = 'list-restaurant-container column';
 
   const image = document.createElement('img');
   image.className = 'list-restaurant-img';
