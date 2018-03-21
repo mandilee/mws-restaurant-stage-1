4a9de360d7532ef486b2c7af1c4a034a1b4225e0
Mandi Lee <github@mandilee.co.uk>
2018-03-21 21:23:18 +0000

    feat: swap google normalize.css to cloudflare

--- sw.js
+++ sw.js
@@ -15,7 +15,8 @@ var urlsToCache = [
     '/data/restaurants.json',
     '/js/restaurant_info.js',
     '/js/dbhelper.js',
-    '/js/main.js'
+    '/js/main.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css'
 ];
 
 self.addEventListener('install', function (event) {
@@ -26,8 +27,11 @@ self.addEventListener('install', function (event) {
     );
 });
 
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
+        // open the cache so we can pull from it if possible
+        // or save the page if it isn't already cached
         caches.open(staticCacheName).then(function (cache) {
             return cache.match(event.request).then(function (response) {
                 return response || fetch(event.request).then(function (response) {
@@ -37,4 +41,10 @@ self.addEventListener('fetch', function (event) {
             })
         })
     )
+});
+
+addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+        return this.skipWaiting();
+    }
 });
