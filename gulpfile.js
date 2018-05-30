7f386a49fd0e1596c5c4234702a4c1a60d8e23f4
Mandi Lee <github@mandilee.co.uk>
2018-05-30 16:01:37 +0100

    feat: gulp images - auto-create resized images and also save as .webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,87 @@
+const SOURCE_JS = 'scripts',
+    SOURCE_SCSS = 'sass',
+    SOURCE_IMG = 'img',
+    DEST_JS = 'js',
+    DEST_CSS = 'css',
+    DEST_IMG = 'img/web';
+
+
+
 var gulp = require('gulp'),
     del = require('del'),
     sass = require('gulp-sass'),
     cleanCss = require('gulp-clean-css'),
-    concat = require('gulp-concat')
-    ;
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    jimp = require('gulp-jimp-resize'),
+    webp = require('gulp-webp');
+
 
 gulp.task('default', function () {
     console.log('eh?');
 });
 
 
+/* combine and compress CSS */
 gulp.task('styles', function () {
-    del(['./css/**']);
+    del([`${DEST_CSS}/**`]);
 
-    gulp.src('sass/**/*.scss')
+    gulp.src(`${SOURCE_SCSS}/**/*.scss`)
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('all.css'))
-        .pipe(cleanCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('css'));
+        .pipe(cleanCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest(DEST_CSS));
 });
 
 
+/* combine javascript files - one for each page since they */
 gulp.task('scripts', function () {
-    del(['./js/**']);
+    del([`${DEST_JS}/**`]);
 
-    gulp.src(['scripts/**/*.js', '!scripts/restaurant_info.js'])
+    gulp.src([`${SOURCE_JS}/**/*.js`, `!${SOURCE_JS}/restaurant_info.js`])
         .pipe(concat('all_index.js'))
-          .pipe(gulp.dest('js'));
+        .pipe(gulp.dest(DEST_JS));
 
-    gulp.src(['scripts/**/*.js', '!scripts/main.js'])
+    gulp.src([`${SOURCE_JS}/**/*.js`, `!${SOURCE_JS}/main.js`])
         .pipe(concat('all_restaurant.js'))
-          .pipe(gulp.dest('js'));
+        .pipe(gulp.dest(DEST_JS));
 });
 
 
+/* resize images */
+gulp.task('resize_images', function () {
+    del([`${DEST_IMG}/*.jpg]`]);
+
+    return gulp.src(`${SOURCE_IMG}/*.jpg`)
+        .pipe(imagemin())
+        .pipe(gulp.dest(DEST_IMG))
+        .pipe(jimp({
+            sizes: [
+                {"suffix": "md", "width": 600},
+                {"suffix": "sm", "width": 300}
+            ]
+        }))
+        .pipe(gulp.dest(DEST_IMG));
+
+});
+/* save images as webp */
+gulp.task('webp_images', ['resize_images'], function () {
+    del([`${DEST_IMG}/*.webp]`]);
+
+    return gulp.src(`${DEST_IMG}/*.jpg`)
+        .pipe(webp())
+        .pipe(gulp.dest(DEST_IMG));
+});
+
+gulp.task('images', ['resize_images', 'webp_images']);
 
 
-gulp.task('cleanup', function() {
-    del(['./css/**']);
-    del(['./js/**']);
+/* cleanup before comitting changes! */
+gulp.task('cleanup', function () {
+    del([`${DEST_CSS}/**`]);
+    del([`${DEST_JS}/**`]);
+    del([`${DEST_IMG}/**`]);
 
 });
